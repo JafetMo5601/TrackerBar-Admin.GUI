01b1fbf69dc683a964fbbccd8a7d73a4a138030d
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-03-14 21:27:07 -0600

    Create the base of the auth page

--- src/app/components/admin/auth/auth.component.ts
+++ src/app/components/admin/auth/auth.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  tryingLoggin: boolean = true;
+  show_register: boolean = false;
+  signUpButton: any;
+  signInButton: any;
+  container: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.signUpButton = document.getElementById("signUp");
+    this.signInButton = document.getElementById("signIn");
+    this.container = document.getElementById("container");
+
+    this.signUpButton.addEventListener("click", () => {
+      this.container.classList.add("right-panel-active");
+    });
+
+    this.signInButton.addEventListener("click", () => {
+      this.container.classList.remove("right-panel-active");
+    });
   }
 
 }
--- src/app/components/admin/auth/register/register.component.ts
+++ src/app/components/admin/auth/register/register.component.ts
@@ -1,15 +1,72 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomPopUpService } from 'src/app/shared/custom-pop-up/custom-pop-up.service';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  signUpForm = new FormGroup({
+    name: new FormControl('', {
+      validators: Validators.required
+    }),
+    last: new FormControl('', {
+      validators: Validators.required
+    }),
+    username: new FormControl('', {
+      validators: Validators.required
+    }),
+    email: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.email
+      ]
+    }),
+    password: new FormControl('', {
+      validators: Validators.required
+    })
+  });
 
-  ngOnInit(): void {
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+  constructor(
+    private customPopUpService: CustomPopUpService,
+    private authService: AuthService
+  ) { }
+
+  public openCustomPopUp(message: string) {
+    this.customPopUpService.confirm(
+      'New user', 
+      message,
+      'auth/login'
+      );
+  }
+
+  onSubmit() {
+    this.authService.register(
+      this.signUpForm.controls['name'].value,
+      this.signUpForm.controls['last'].value,
+      this.signUpForm.controls['username'].value,
+      this.signUpForm.controls['email'].value,
+      this.signUpForm.controls['password'].value
+    ).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+        this.openCustomPopUp(data.message);
+      }, err => {
+        console.log(err);
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
   }
 
 }
