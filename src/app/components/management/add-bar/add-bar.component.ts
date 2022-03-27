260737c5901d8dcc182408235cf3716a07893994
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-03-26 21:56:43 -0600

    Initialize the component to create a new bar

---
+++ src/app/components/management/add-bar/add-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-bar',
+  templateUrl: './add-bar.component.html',
+  styleUrls: ['./add-bar.component.css']
+})
+export class AddBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
