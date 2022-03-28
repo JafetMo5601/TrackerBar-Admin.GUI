a52e9eca2bc6420f6cf119c0336cfe1be01b6950
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-03-28 10:04:35 -0600

    Fix the pointer of the bar details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { ManagementComponent } from './components/management/management.component';
 import { HomeComponent } from './components/home/home.component';
 import { EditComponent } from './components/profile/edit/edit.component';
+import { UpdateBarComponent } from './components/management/update-bar/update-bar.component';
 
 
 const routes: Routes = [
@@ -14,7 +15,7 @@ const routes: Routes = [
    { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
    { path: 'profile/:user_id', component: EditComponent, canActivate: [AuthGuard] },
    { path: 'admin-restaurants', component: ManagementComponent, canActivate: [AuthGuard] },
-   { path: 'restaurant/:restaurant_id', component: ManagementComponent, canActivate: [AuthGuard] },
+   { path: 'restaurant/:restaurant_id', component: UpdateBarComponent, canActivate: [AuthGuard] },
    { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
    { path: '', redirectTo: 'home', pathMatch: 'full' },
    { path: '**', component: NotFoundComponent }
