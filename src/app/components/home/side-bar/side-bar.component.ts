50d6904980cd4e4cdcaed2c52740f162b1bd1b96
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-04-26 17:53:20 -0600

    Enable the change of restaurants in the home side bar.

--- src/app/components/home/side-bar/side-bar.component.ts
+++ src/app/components/home/side-bar/side-bar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Bares } from 'src/app/shared/interfaces/bares';
+import { AdminBaresService } from 'src/app/shared/services/admin-bares.service';
 import { BarStatisticsService } from 'src/app/shared/services/bar-statistics.service';
+import { PersonalInformationService } from 'src/app/shared/services/personal-information.service';
 
 @Component({
   selector: 'app-side-bar',
@@ -7,30 +10,62 @@ import { BarStatisticsService } from 'src/app/shared/services/bar-statistics.ser
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent implements OnInit {
-
+  user_id: string | null = '';
   total_tables: number = 0;
   tables_available: number = 0;
   total_people: number = 0;
   spaces_available: number = 0;
-
-  constructor(private bar_stats: BarStatisticsService) { }
+  restaurant_id: number = 10;
+  bares: Array<Bares> = [
+    {
+      id: 7,
+      name: 'test'
+    },
+    {
+      id: 2,
+      name: 'test'
+    },
+    {
+      id: 10,
+      name: 'test'
+    },
+  ]
+  constructor(
+    private bar_stats: BarStatisticsService,
+    private adminBarService: AdminBaresService,
+    private userInfo: PersonalInformationService
+    ) { }
 
   ngOnInit(): void {
-    this.bar_stats.getTotalTables(1).subscribe (
+    this.retrieveRestaurantValues();
+    this.user_id = this.userInfo.getId();
+
+    if (this.user_id != null) {
+      this.adminBarService.getRestaurantsByOwner(this.user_id).subscribe(
+        data => {
+          this.bares = data
+        },
+        err => {}
+      );
+    }
+  }
+
+  retrieveRestaurantValues() {
+    this.bar_stats.getTotalTables(this.restaurant_id).subscribe (
       data => {this.total_tables = data},
-      err => {console.log(err)});
+      err => {this.total_tables = 0});
 
-    this.bar_stats.getTotalTablesAvailable(1).subscribe (
+    this.bar_stats.getTotalTablesAvailable(this.restaurant_id).subscribe (
       data => {this.tables_available = data},
-      err => {console.log(err)});
+      err => {this.tables_available = 0});
       
-    this.bar_stats.getTotalPeople(1).subscribe (
+    this.bar_stats.getTotalPeople(this.restaurant_id).subscribe (
       data => {this.total_people = data},
-      err => {console.log(err)});
+      err => {this.total_people = 0});
 
-    this.bar_stats.getTotalSpacesAvailable(1).subscribe (
+    this.bar_stats.getTotalSpacesAvailable(this.restaurant_id).subscribe (
       data => {this.spaces_available = data},
-      err => {console.log(err)})
+      err => {this.spaces_available = 0});
   }
 
 }
